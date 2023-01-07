5a1a7c9cffcafba93b7954619ebb233087e12196
skillbox-koutja <skillbox-koutja@yandex.ru>
2023-01-07 19:16:42 +0300

    4.4.1 AppBar + Text/TextStyle

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -10,8 +10,30 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        toolbarHeight: 72 + 64,
+        title: const Padding(
+          padding: EdgeInsets.fromLTRB(16, 64, 16, 0),
+          child: SizedBox(
+            height: 72,
+            child: Text(
+              'Список\nинтересных мест',
+              style: TextStyle(
+                color: Color(0xFF252849),
+                fontSize: 32.0,
+                height: 1.12,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w700,
+              ),
+              maxLines: 2,
+            ),
+          ),
+        ),
+      ),
+      body: const Center(
         child: Padding(
           padding: EdgeInsets.all(20.0),
           child: TextField(
