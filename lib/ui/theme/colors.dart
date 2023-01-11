bfe7aa5c898ac5179bc25c548d176682dc5110ec
skillbox-koutja <skillbox-koutja@yandex.ru>
2023-01-11 03:04:29 +0300

    Добавлены файлы для описания тем

---
+++ lib/ui/theme/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/painting.dart';
+
+class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const dark = Color(0xFF1A1A20);
+  static const secondary = Color(0xFF3B3E5B);
+  static const secondary2 = Color(0xFF7C7E92);
+  static const inactive = Color.fromRGBO(124, 126, 146, 0.56);
+  // white
+  static const whiteMain = Color(0xFF252849);
+  static const whiteGreen = Color(0xFF4CAF50);
+  static const whiteYellow = Color(0xFFFCDD3D);
+  static const whiteRed = Color(0xFFEF4343);
+  // black
+  static const blackMain = Color(0xFF21222C);
+  static const blackGreen = Color(0xFF6ADA6F);
+  static const blackYellow = Color(0xFFFFE769);
+  static const blackRed = Color(0xFFCF2A2A);
+}
