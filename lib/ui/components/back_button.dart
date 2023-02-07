ca9204fe91a89c92bfd654c474520eb30e68533b
skillbox-koutja <skillbox-koutja@yandex.ru>
2023-02-07 07:30:47 +0300

    6.6.1 Схлопывающаяся галерея

---
+++ lib/ui/components/back_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:places/assets/theme/colors.dart';
+import 'package:places/ui/icons/svg_icons.dart';
+
+class AppBackButton extends StatelessWidget {
+  const AppBackButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorsTheme = theme.extension<CustomColors>();
+
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pop();
+      },
+      child: SizedBox.fromSize(
+        size: const Size.square(32),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: colorsTheme?.iconBackground,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: ChevronLeftSvgIcon(
+            color: colorsTheme?.icon,
+          ),
+        ),
+      ),
+    );
+  }
+}
